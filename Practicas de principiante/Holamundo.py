8b5b58a321da79aa10de4a79947ce89a20e99307
unknown <leonardoa173@gmail.com>
2022-06-08 19:52:02 -0300

    Primer dia de practica

---
+++ Practicas de principiante/Holamundo.py
@@ -0,0 +1,17 @@
+#imprecion por texto
+
+print("hola mundo")
+
+#insercion de datos
+
+dato = input("ingresa un dato")
+
+#convertores
+
+entero = (int(input("ingresa un numero")))
+
+flotante = (float(input("ingresa un flotante")))
+
+cadena = (str(input("Ingresa un a valor")))
+
+#
